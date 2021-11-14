13fced935ca566e1f0a82c863aa686252144c892
SarathLUN <lun.sarath@gmail.com>
2021-11-14 20:23:22 +0700

    component selector by attribute

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  selector: '[app-servers]',
   // templateUrl: './servers.component.html',
   template: `
-    <app-server></app-server>
-    <app-server></app-server>
+    <p>Using attribute selector.</p>
     <hr>
   `,
   styleUrls: ['./servers.component.css']
