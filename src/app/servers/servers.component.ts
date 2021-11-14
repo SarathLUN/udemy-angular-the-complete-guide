30e26d7323f02dad8920dbc07b7b88c67108cc97
SarathLUN <lun.sarath@gmail.com>
2021-11-14 18:37:44 +0700

    022 Working with Component Templates

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,7 +2,12 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  // templateUrl: './servers.component.html',
+  template: `
+    <app-server></app-server>
+    <app-server></app-server>
+    <hr>
+  `,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
