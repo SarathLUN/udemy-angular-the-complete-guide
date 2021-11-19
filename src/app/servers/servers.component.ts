f2a410a583f03452d64316fa0b04083940571480
SarathLUN <lun.sarath@gmail.com>
2021-11-19 20:28:05 +0700

    completed video 030 Event Binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreationStatus = "No server was created!";
   constructor() {
     setTimeout(()=>{
       this.allowNewServer = true;
@@ -16,4 +17,7 @@ export class ServersComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onCreateServer(){
+    this.serverCreationStatus = "Server was created!";
+  }
 }
