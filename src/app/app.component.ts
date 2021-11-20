86a4517ccc254c28cb487999df9be346545b3afc
SarathLUN <lun.sarath@gmail.com>
2021-11-20 16:43:17 +0700

    my solution on 036-Assignment-Practicing-Databinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  userName = "";
+  isEnableBtnResetUserName = true;
 
+  // check if userName is not empty
+  onUpdateUserName(event: Event) {
+    console.log(event);
+    this.isEnableBtnResetUserName = (<HTMLInputElement>event.target).value === "";
+  }
+
+  onResetUserName() {
+    this.userName = "";
+    this.isEnableBtnResetUserName = true;
+  }
 }
