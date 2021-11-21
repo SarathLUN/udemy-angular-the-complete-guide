76b2d776711625f7d47cf6f07d60b51224c576ad
SarathLUN <lun.sarath@gmail.com>
2021-11-21 10:59:31 +0700

    completed video #040-Styling-Elements-Dynamically-with-ngStyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,12 +2,21 @@ import {Component} from "@angular/core";
 
 @Component({
   selector: 'app-server',
-  templateUrl:'./server.component.html'
+  templateUrl: './server.component.html'
 })
-export class ServerComponent{
-  serverID: number = 19;
+export class ServerComponent {
+  serverID: number = 34;
   serverStatus: string = "Online";
-  getServerStatus(){
+
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'Online' : 'Offline';
+  }
+
+  getServerStatus() {
     return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'Online' ? 'green' : 'red';
   }
 }
