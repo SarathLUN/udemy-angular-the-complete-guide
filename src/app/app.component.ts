27555061cdb536d104f9ebd5ed8ad91e1683ca06
SarathLUN <lun.sarath@gmail.com>
2021-11-21 14:58:53 +0700

    completed assignment video #043-Assignment-Practicing-Directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  showParagraph = false;
+  myLogs:Array<any> = [];
 
+  onToggleDetail() {
+    this.showParagraph = !this.showParagraph;
+    const items = this.myLogs.length + 1;
+    this.myLogs.push(items);
+    console.log(this.myLogs);
+  }
 }
