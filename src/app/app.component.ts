5f30f512b9934d57acaa837e4ec6628fa82c657d
SarathLUN <lun.sarath@gmail.com>
2021-11-27 09:03:06 +0700

    completed video #067_Binding_to_Custom_Properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [];
+  serverElements = [
+    {type:"server", name:"test server", content: "just a test!"}
+  ];
 
 
 
