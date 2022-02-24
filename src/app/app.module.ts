697bdc4b0352ad1df1468c4a652cf57e1cbd0576
SarathLUN <lun.sarath@gmail.com>
2022-02-24 23:55:51 +0700

    completed video 094-Creating-a-Basic-Attribute-Directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
+import {BasicHighlightDirective} from "./basic-highlight/basic-highlight.directive";
 
 
 @NgModule({
 	declarations: [
 		AppComponent,
+		BasicHighlightDirective
 	],
 	imports: [
 		BrowserModule,
