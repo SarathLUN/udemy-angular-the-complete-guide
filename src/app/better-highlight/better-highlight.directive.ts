eff8cd4ff636c7275c4bfae36c26c59aeae33c06
SarathLUN <lun.sarath@gmail.com>
2022-02-27 12:12:45 +0700

    completed video 098-Using-HostBinding-to-Bind-to-Host-Properties, optimized imports.

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,11 +1,11 @@
-import {Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2} from '@angular/core';
+import {Directive, HostBinding, HostListener, OnInit} from '@angular/core';
 
 @Directive({
 	selector: '[appBetterHighlight]'
 })
-export class BetterHighlightDirective implements OnInit{
+export class BetterHighlightDirective implements OnInit {
 
-	constructor(private elRef: ElementRef, private renderer: Renderer2) {
+	constructor() {
 	}
 
 	ngOnInit(): void {
@@ -14,16 +14,12 @@ export class BetterHighlightDirective implements OnInit{
 	@HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
 	@HostBinding('style.color') color: string = 'black';
 
-	@HostListener('mouseenter') onMouseOver(eventData: Event){
-		// this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
-		// this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white');
+	@HostListener('mouseenter') onMouseOver(eventData: Event) {
 		this.backgroundColor = 'blue';
 		this.color = 'white';
 	}
 
-	@HostListener('mouseleave') onMouseLeave(eventData: Event){
-		// this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
-		// this.renderer.setStyle(this.elRef.nativeElement, 'color', 'black');
+	@HostListener('mouseleave') onMouseLeave(eventData: Event) {
 		this.backgroundColor = 'transparent';
 		this.color = 'black';
 	}
